85f359d49268565ec3175f2c6463ee6a86eae80d
adamkong1995 <adamkong256@gmail.com>
2019-07-02 11:20:23 +0800

    Updated for linting

--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import "./index.css";
 
 interface Props {
-    checked: boolean;
-    onChange: (checked: boolean) => void;
-    disabled?: boolean;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  disabled?: boolean;
 }
 
 export default class Switch extends React.PureComponent<Props> {
-    onClick = () => {this.props.onChange(!this.props.checked)}
+  onClick = () => {
+    this.props.onChange(!this.props.checked);
+  };
 
-    render() {
-        return (
-            <div className="comp-switch">
-                <input 
-                    disabled={this.props.disabled}
-                    onClick={this.onClick} 
-                    type='checkbox'
-                />
-            </div>
-        );
-    };
-};
+  render() {
+    return (
+      <div className="comp-switch">
+        <input
+          disabled={this.props.disabled}
+          onClick={this.onClick}
+          type="checkbox"
+        />
+      </div>
+    );
+  }
+}

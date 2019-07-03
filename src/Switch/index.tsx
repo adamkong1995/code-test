8ac229b106e980139cd6e094221beea92687c5e1
adamkong1995 <adamkong256@gmail.com>
2019-07-03 14:52:02 +0800

    Updated per feedback

--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -2,20 +2,30 @@ import React from "react";
 import "./index.css";
 
 interface Props {
+    controlled: boolean;
     checked: boolean;
     onChange: (checked: boolean) => void;
     disabled?: boolean;
 }
 
 export default class Switch extends React.PureComponent<Props> {
+    private input: React.RefObject<HTMLInputElement>;
+    constructor(props: Props) {
+        super(props);
+        this.input = React.createRef();
+    }
+
     onClick = () => {
-        this.props.onChange(!this.props.checked);
+        if (this.props.controlled) {
+            return this.props.onChange(!this.props.checked);
+        }
+        return this.props.onChange(this.input.current!.checked);
     };
 
     render() {
         return (
             <div className="comp-switch">
-                <input disabled={this.props.disabled} onClick={this.onClick} type="checkbox" />
+                <input disabled={this.props.disabled} ref={this.input} onClick={this.onClick} type="checkbox" />
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Switch from "./Switch";
-import Uncontrolled from "./Switch/Uncontrolled";
 
 interface State {
     isSwitchChecked: boolean;
     isSwitchDisabled: boolean;
-    isSwitchUncontrolled: boolean;
+    isSwitchControlled: boolean;
 }
 
 class App extends React.PureComponent<{}, State> {
     public state: State = {
         isSwitchChecked: false,
         isSwitchDisabled: false,
-        isSwitchUncontrolled: false,
+        isSwitchControlled: true,
     };
 
     onToggleDisable = () => this.setState({isSwitchDisabled: !this.state.isSwitchDisabled});
 
     onChange = (checked: boolean) => this.setState({isSwitchChecked: checked});
 
-    onToggleUncontrolled = () => this.setState({isSwitchUncontrolled: !this.state.isSwitchUncontrolled});
-
-    renderContent() {
-        if (this.state.isSwitchUncontrolled) {
-            return <Uncontrolled disabled={this.state.isSwitchDisabled} />;
-        }
-        return <Switch checked={this.state.isSwitchChecked} disabled={this.state.isSwitchDisabled} onChange={this.onChange} />;
-    }
+    onToggleControlled = () => this.setState({isSwitchControlled: !this.state.isSwitchControlled});
 
     render() {
         return (
             <div>
                 <div>
-                    {this.renderContent()}
+                    <Switch controlled={this.state.isSwitchControlled} checked={this.state.isSwitchChecked} disabled={this.state.isSwitchDisabled} onChange={this.onChange} />
                     <button type="button" onClick={this.onToggleDisable}>
                         Toggle Disable
                     </button>
                 </div>
                 <div>
-                    <button type="button" onClick={this.onToggleUncontrolled}>
+                    <button type="button" onClick={this.onToggleControlled}>
                         Toggle Uncontrolled
                     </button>
-                    <div>{this.state.isSwitchUncontrolled ? "Un-Controlled" : "Controlled"}</div>
+                    <div>{this.state.isSwitchControlled ? "Controlled" : "Un-Controlled"}</div>
                 </div>
             </div>
         );
